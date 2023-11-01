c744b464fa7239ad8ba2784fa96e10189f9616a8
sathagesey33 <abdukadirmuhanad@gmail.com>
2023-11-01 00:30:53 -0700

    Is perfect

--- binary_trees.h
+++ binary_trees.h
@@ -38,6 +38,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
 
 /* helper function */
 void binary_tree_print(const binary_tree_t *tree);
