c881d1f7456d0762a9de435bd01de34510986a6f
sathagesey33 <abdukadirmuhanad@gmail.com>
2023-11-02 08:28:46 -0700

    uncle

--- binary_trees.h
+++ binary_trees.h
@@ -43,6 +43,7 @@ int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int));
 size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
+binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
 /* helper function */
 void binary_tree_print(const binary_tree_t *tree);
